2fe8e1cb6ebeff88ed8fdecb0d4b9dcad0b70441
Syugen <syugen.zhou@gmail.com>
2017-09-08 22:17:36 -0400

    Improved performance in spacial cases

--- assets/scripts/video.js
+++ assets/scripts/video.js
@@ -1,21 +1,58 @@
 'use strict';
 
+/* Case tree:
+ * if (1. B站 HTTP 回应代码 == 200 且
+ *     2. B站 API 中 stat.code == 0) // 视频信息找到
+ *     从B站网页读取更多信息
+ *     if (读取失败) 上传时间、分类、UP主签名 改为 “读取失败”
+ *     else
+ *         添加上传时间、分类、UP主签名信息
+ *         if (1. 唧唧 HTTP 回应代码 ！= 200 或
+ *             2. 唧唧 API 回应代码 != 0 或 // 可能是UP主禁止
+ *             2. 唧唧 API 信息显示视频删除) // 一定是唧唧缓存过期
+ *             将标题、简介、UP用户名、UP头像更新为网页摘取内容
+ *         else 以上四项不变（即相信唧唧缓存正确）
+ * else
+ *     该 JS 不会被加载。不会请求唧唧
+ */
+
 $.get("/page?aid=" + $("#aid").val(), function(data) {
-    //$("#title").text(data.title);
-    $("#time").text(data.time);
-    $("#class").text(data.class1 + " > " + data.class2 + " > " + data.class3);
-    //$("#description").text(data.description);
-    //$("#upName").text(data.upName);
-    $("#upSign").text(data.upSign);
-    //$("#upAvatar").attr("src", data.upAvatar);
+    // TODO handle non-200 error
+    if (data.error) { // Parsing时出错
+        $("#time").text("读取上传时间失败");
+        $("#class").text("读取分类失败");
+        $("#upSign").text("读取UP主签名失败");
+        // Parsing错误一般仅发生在番剧和视频不存在的情况，而视频不存在时页面不加载，
+        // 所以基本可以断定是番剧导致的，推断其特点为唧唧API返回空标题
+        if ($("#title").text() == "") {
+            document.title = "Bilibili API";
+            $("#title").text("读取标题失败");
+            $("#subtitle").text("这可能是某个番剧的一集，还没想到根据av号判断番剧的方法");
+            $("#img").attr("alt", "读取缩略图失败");
+        }
+    } else {
+        $("#time").text(data.time);
+        $("#class").text(data.class1 + " > " + data.class2 + " > " + data.class3);
+        $("#upSign").text(data.upSign);
+        if ($("#jijifail").val() == 1 || $("#title").text().trim() == "该视频已被B站删除") {
+            document.title = data.title + " - Bilibili API";
+            $("#title").text(data.title);
+            $("#description").text(data.description);
+            $("#upName").text(data.upName);
+            $("#upAvatar").attr("src", data.upAvatar);
+            $("#img").attr("alt", "读取缩略图失败");// 是否可以从网页读取缩略图？
+        }
+    }
+
 });
 
 var bot = $("#stat").position().top + $("#stat").height();
 $("#list").height(bot - $("#list").position().top);
-$("li").width($("ol:first").width() / 2 - 5).css("float", "left");
+$("li").css("margin-left", "30px");
+$("li").width($("ol:first").width() / 2 - 60).css("float", "left");
 
 $( window ).resize(function() {
     var bot = $("#stat").position().top + $("#stat").height();
     $("#list").height(bot - $("#list").position().top);
-    $("li").width($("ol:first").width() / 2 - 5);
+    $("li").width($("ol:first").width() / 2 - 60);
 });
--- biliInfo.js
+++ biliInfo.js
@@ -35,17 +35,17 @@ app.get("/video", function(req, res) {
 
     var req2 = http.get(options, function(data) {
         if (data.statusCode != 200) {
-            return res.send("Error while visiting bilibili.com. " +
-                            "Status code is: " + data.statusCode);
+            return res.render("video.html", {errorCode: data.statusCode});
         }
         var output = "";
         data.on('data', function(chunk) {
             output += chunk;
         }).on('end', function() {
             var stat = JSON.parse(output);
+            if (stat.code != 0) {
+                return res.render("video.html", {stat: stat});
+            }
             getInfo(req, res, stat);
-        }).on('error', function(e) {
-            console.log('ERROR: ' + e.message);
         });
     });
 });
@@ -57,21 +57,22 @@ function getInfo(req, res, stat) {
     };
 
     var req2 = http.get(options, function(data) {
+        var settings = {
+            stat: stat,
+            isSubpage: true,
+            scripts: ["video"]
+        };
+        if (data.statusCode != 200) {
+            return res.render("video.html", settings);
+        }
         var output = "";
         data.on('data', function(chunk) {
             output += chunk;
         }).on('end', function() {
             var info = JSON.parse(output);
             info.time = new Date(info.time * 1000); // cache time
-            var settings = {
-                stat: stat,
-                info: info,
-                isSubpage: true,
-                scripts: ["video"]
-            };
+            settings.info = info;
             return res.render("video.html", settings);
-        }).on('error', function(e) {
-            console.log('ERROR: ' + e.message);
         });
     });
 }
@@ -93,19 +94,23 @@ app.get("/page", function(req, res) {
             output += chunk.toString();
         }).on('end', function() {
             var dom = parser.parseFromString(output), json = {};
-            var tm = dom.getElementsByClassName("tminfo")[0];
-            //json.title = dom.getElementsByTagName("h1")[0].getAttribute("title");
-            json.time = dom.getElementsByTagName("time")[0].getAttribute("datetime");
-            json.class1 = tm.getElementsByTagName("a")[0].innerHTML;
-            json.class2 = tm.getElementsByTagName("a")[1].innerHTML;
-            json.class3 = tm.getElementsByTagName("a")[2].innerHTML;
-            //json.description = dom.getElementById("v_desc").innerHTML;
-            //json.upName = dom.getElementsByClassName("usname")[0].
-            //                  getElementsByTagName("a")[0].innerHTML;
-            json.upSign = dom.getElementsByClassName("sign")[0].innerHTML;
-            //json.upAvatar = dom.getElementsByClassName("upinfo")[0].
-            //                    getElementsByTagName("img")[0].getAttribute("data-fn-src");
-            return res.send(json);
+            try {
+                var tm = dom.getElementsByClassName("tminfo")[0];
+                json.title = dom.getElementsByTagName("h1")[0].getAttribute("title");
+                json.time = dom.getElementsByTagName("time")[0].getAttribute("datetime");
+                json.class1 = tm.getElementsByTagName("a")[0].innerHTML;
+                json.class2 = tm.getElementsByTagName("a")[1].innerHTML;
+                json.class3 = tm.getElementsByTagName("a")[2].innerHTML;
+                json.description = dom.getElementById("v_desc").innerHTML;
+                json.upName = dom.getElementsByClassName("usname")[0].
+                                  getElementsByTagName("a")[0].innerHTML;
+                json.upSign = dom.getElementsByClassName("sign")[0].innerHTML;
+                json.upAvatar = dom.getElementsByClassName("upinfo")[0].
+                                    getElementsByTagName("img")[0].getAttribute("data-fn-src");
+                return res.send(json);
+            } catch(err) {
+                return res.send({error: true, data: output});
+            }
         }).on('error', function(e) {
             console.log('ERROR: ' + e.message);
         });
