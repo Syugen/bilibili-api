19e47f84abf1c04ca5e352e9ad98b6b47ed870b4
Syugen <syugen.zhou@gmail.com>
2017-09-11 14:04:32 -0400

    User page css

--- assets/scripts/user_basics.js
+++ assets/scripts/user_basics.js
@@ -4,9 +4,29 @@ var uid = $("#uid").val();
 var type = $("#type").val();
 
 $.get("/biliapi_userinfo?uid=" + uid, function(result) {
-    $("#title").text(result.data.name);
+    $("#title").text(result.data.name);console.log(result.data)
     if (type == "info") {
-        $("tbody").text("调整一下格式！\n"+JSON.stringify(result.data))
+            //var d = new Date(1000 * result.data.list[i].mtime);
+        var markup = "";
+        var d = new Date(1000 * result.data.regtime);
+        markup += "<tr><td>" + "性别" + "</td><td>" + result.data.sex + "</td></tr>";
+        markup += "<tr><td>" + "生日" + "</td><td>" + result.data.birthday + "</td></tr>";
+        markup += "<tr><td>" + "地点" + "</td><td>" + result.data.place + "</td></tr>";
+        markup += "<tr><td>" + "播放数" + "</td><td>" + result.data.playNum + "</td></tr>";
+        markup += "<tr><td>" + "排名" + "</td><td>" + result.data.rank == 10000 ? ">=" : "" + 
+                                                      result.data.rank + "</td></tr>";
+        markup += "<tr><td>" + "注册时间" + "</td><td>" + d.getFullYear() + "-" + (d.getMonth() + 1) +
+                                                    "-" + d.getDate() + "</td></tr>";
+        markup += "<tr><td>" + "" + "</td><td>" + result.data + "</td></tr>";
+        markup += "<tr><td>" + "" + "</td><td>" + result.data + "</td></tr>";
+        markup += "<tr><td>" + "" + "</td><td>" + result.data + "</td></tr>";
+        markup += "<tr><td>" + "" + "</td><td>" + result.data + "</td></tr>";
+        markup += "<tr><td>" + "" + "</td><td>" + result.data + "</td></tr>";
+            $("table tbody").append(markup);
+        $("#upAvatar").attr("src", result.data.face);
+        $("#upName").text(result.data.name);
+        $("#upSign").text(result.data.sign);
+
     }
 });
 
--- routes/user.js
+++ routes/user.js
@@ -8,23 +8,24 @@ var parser = new DomParser();
 exports.getUserInfo = function(req, res) {
     if (!req.query.uid) return res.render("error", {queryError: "uid"});
     else return res.render("user", {type: "info", uid: req.query.uid,
-                                      scripts: ["user_basics", "user_info"]});
+                                    scripts: ["user_basics", "user_info"],
+                                    styles: ["user_info"]});
 }
 
 exports.getUserVideoList = function(req, res) {
     if (!req.query.uid) return res.render("error", {queryError: "uid"});
     else return res.render("user", {type: "video", uid: req.query.uid,
-                                      scripts: ["user_basics", "user_video"]});
+                                    scripts: ["user_basics", "user_video"]});
 }
 
 exports.getUserFollowing = function(req, res) {
     if (!req.query.uid) return res.render("error", {queryError: "uid"});
     else return res.render("user", {type: "followings", uid: req.query.uid,
-                                      scripts: ["user_basics", "follow"]});
+                                    scripts: ["user_basics", "follow"]});
 }
 
 exports.getUserFollower = function(req, res) {
     if (!req.query.uid) return res.render("error", {queryError: "uid"});
     else return res.render("user", {type: "followers", uid: req.query.uid,
-                                      scripts: ["user_basics", "follow"]});
+                                    scripts: ["user_basics", "follow"]});
 }
